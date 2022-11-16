501b7ec1aaa9d1729341fe7888fda4a9483ba960
Tony-Marko <101558697+Tony-Marko@users.noreply.github.com>
2022-11-16 08:30:07 +0700

    Shuffling Deck works

---
+++ classes/deck.py
@@ -0,0 +1,54 @@
+from . import card
+import random
+
+class Deck:
+    def __init__( self, num_of_decks):
+        suits = [ "spades" , "hearts" , "clubs" , "diamonds" ]
+        self.num_of_decks = num_of_decks #specify number of decks
+        self.total = num_of_decks*52
+        self.cards = []
+
+        for x in range(num_of_decks):
+            for s in suits:
+                for i in range(1,14):
+                    str_val = ""
+                    if i == 1:
+                        str_val = "Ace"
+                        pnt_val = 1
+                    elif i == 11:
+                        str_val = "Jack"
+                        pnt_val = 10
+                    elif i == 12:
+                        str_val = "Queen"
+                        pnt_val = 10
+                    elif i == 13:
+                        str_val = "King"
+                        pnt_val = 10
+                    else:
+                        str_val = str(i)
+                        pnt_val = i
+                    self.cards.append( card.Card( s , pnt_val , str_val ) )
+
+    def show_cards(self):
+        for card in self.cards:
+            card.card_info()
+        print (f'Cards in Deck: {len(self.cards)}')
+
+    def shuffle_cards(self):
+        random.shuffle(self.cards) #shuffle randomizes and replaces the list, no need for useing return
+        
+
+    @classmethod
+    def deal_cards(cls):
+        #get random card
+        pass
+
+class Dealer:
+    def __init__(self):
+        self.dealerscards = []
+
+class Player:
+    def __init__(self, name, balance):
+        self.name = name # players name
+        self.balance = balance #players balance
+        self.playerscards = []
---
+++ game.py
@@ -0,0 +1,12 @@
+from classes.deck import Deck, Dealer, Player
+
+#How many decks to use:
+num_of_decks = 3
+#Full deck to create
+bicycle = Deck(num_of_decks)
+
+bicycle.shuffle_cards()
+
+bicycle.show_cards()
+
+
